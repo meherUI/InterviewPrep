d68e5bdffc7dec9e82f16eb36bf436efd0cb8dd1
Thatavarthi <ThataMeh@ES.AD.ADP.COM>
2021-06-30 13:07:41 +0530

    Misc

--- Algorithms/Others/LongestCommonPrefix.js
+++ Algorithms/Others/LongestCommonPrefix.js
@@ -1,12 +1,12 @@
 // https://www.youtube.com/watch?v=gJOgI8TpSgc&list=PLZlA0Gpn_vH8ShxSPO48q8u0SeQBuCdnN&index=2
-let longestCommonPrefix = function(strs){
+let longestCommonPrefix = function(arr){
     let prefix = ''
-    if(strs.length === 0) return prefix
+    if(arr.length === 0) return prefix
 
-    for(var i=0; i<strs[0].length; i++){
-        const character = strs[0][i]
-        for(var j=1; j<strs.length; j++){
-            if(strs[j][i] !== character) return prefix            
+    for(var i=0; i<arr[0].length; i++){
+        const character = arr[0][i]
+        for(var j=1; j<arr.length; j++){
+            if(arr[j][i] !== character) return prefix            
         }
         prefix = prefix + character
     }  
--- Algorithms/Others/MaxSubArraySum.js
+++ Algorithms/Others/MaxSubArraySum.js
@@ -1,8 +1,9 @@
 function MaxSubArraySum(nums, size){
+    if(size <0 || size> nums.length) return null
     let currentSum = 0
     let maxSum = -Infinity
 
-    for(let i=0; i<= nums.length; i++){
+    for(let i=0; i< nums.length; i++){
         currentSum += nums[i]
         if(i >= size-1){
             maxSum = Math.max(currentSum, maxSum)
@@ -12,4 +13,4 @@ function MaxSubArraySum(nums, size){
     return maxSum
 }
 
-console.log([1,2,3,5,4,8,6,2],3)
+console.log(MaxSubArraySum([1,2,3,5,4,8,6,2],3))
